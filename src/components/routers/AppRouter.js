0c29c75809e9e4937ae3ddc44b86cad421f99260
JohannesSchreurs <johannes@basedesign.com>
2019-08-29 15:31:09 +0200

    Changed create to update (as you only create once but update multiple times)

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../home/Home';
 import NotFoundPage from '../routes/notFound/NotFoundPage';
 import SurveyPage from '../routes/survey/SurveyPage';
-import CreateSurveyPage from '../routes/survey/CreateSurveyPage';
 import UpdateSurveyPage from '../routes/survey/UpdateSurveyPage';
 
 const AppRouter = () => (
@@ -12,7 +11,6 @@ const AppRouter = () => (
             <Switch>
                 <Route path="/" component={Home} exact/>
                 <Route path="/survey/:id" component={SurveyPage}/>
-                <Route path="/create/:id" component={CreateSurveyPage} />
                 <Route path="/update/:id" component={UpdateSurveyPage}/>
                 <Route component={NotFoundPage} />
             </Switch>
