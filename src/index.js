959d8aa063ca9c8ea6f7c0516b1f9ebe4094315a
JohannesSchreurs <johannes@basedesign.com>
2019-09-01 23:39:47 +0200

    Added a global entry for the styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import AppRouter from './components/routers/AppRouter';
+import './style/styles.scss';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
