352a4d33672babb16aa9f83a402915432716984c
JohannesSchreurs <johannes@basedesign.com>
2019-09-01 23:40:00 +0200

    Added style for the header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const Header = () => {
 
     return (
-        <Link to="/">Back to home</Link>
+        <div className="banner">
+            <Link className="banner-item" to="/">Back to home</Link>
+        </div>
     )
 }
 
